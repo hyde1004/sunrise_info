80c8519397d431a291cd183df4f99258e3125ed1
Junggu Lee <hyde1117@naver.com>
2014-09-19 04:43:58 +0900

    test_get_html is added

--- test_sunrise_info.py
+++ test_sunrise_info.py
@@ -1,11 +1,19 @@
 import unittest
+import urllib.request
 
 class TestRunRiseInfo(unittest.TestCase):
 	def test_sample(self):
 		self.assertEqual(1, 1)
 
 	def test_get_html(self):
-		self.assertTrue('일출몰' == '일출몰')	
-		
+		url = 'http://astro.kasi.re.kr/Life/Knowledge/sunmoon_map/sunmoon_popup.php?year=2014&month=9&location=%C3%B5%BE%C8'
+		req = urllib.request.Request(url)
+
+		f = urllib.request.urlopen(req)
+		html = f.read().decode('euc-kr')
+		f.close()
+
+		self.assertTrue('일출몰' in html)	
+
 if __name__== '__main__':
 	unittest.main()
