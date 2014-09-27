4775837afcce2e807588172893e5b574a4d1beeb
이정구 <hyde1004@gmail.com>
2014-09-27 13:24:33 +0900

    sucess test_get_data

--- draw_day.py
+++ draw_day.py
@@ -3,16 +3,21 @@ import matplotlib.dates
 import math
 
 class DrawDay():
-	days = None
-	times = None
+	days = []
+	times = []
 
 	def __init__(self):
 		pass
 
 	def get_data(self, day_info):
-		self.days = [math.trunc(matplotlib.dates.date2num(day)) for day in day_info]
-		#self.days = [datetime.date(day.year, day.month, day.day) for day in day_info]
-		self.times = [matplotlib.dates.date2num(day) - math.trunc(matplotlib.dates.date2num(day))  for day in day_info]
-		#return 0
-		#return ([735501, 735502, 735503], [0.2513888889, 0.2527777777, 0.254861111])
+		for day in day_info:
+			info = math.modf(matplotlib.dates.date2num(day))
+
+			self.times.append(info[0])
+			self.days.append(info[1])
+		# self.days = [math.trunc(matplotlib.dates.date2num(day)) for day in day_info]
+		# #self.days = [datetime.date(day.year, day.month, day.day) for day in day_info]
+		# self.times = [matplotlib.dates.date2num(day) - math.trunc(matplotlib.dates.date2num(day))  for day in day_info]
+		# #return 0
+		# #return ([735501, 735502, 735503], [0.2513888889, 0.2527777777, 0.254861111])
 		return (self.days, self.times)
--- test_daw_day.py
+++ test_daw_day.py
@@ -11,7 +11,16 @@ class TestDrawDay(unittest.TestCase):
 		data = [datetime.datetime(2014,9,25, 6, 2), datetime.datetime(2014,9,26, 6,4), datetime.datetime(2014, 9, 27, 6, 7)]
 
 		out = day_info.get_data(data)
-		self.assertEqual(out, ([735501, 735502, 735503], [0.2513888889, 0.2527777777, 0.254861111])) 
+		# self.assertAlmostEqual(out, ([735501, 735502, 735503], [0.2513888889, 0.2527777777, 0.254861111])) 
+		self.assertAlmostEqual(out[0][0], 735501)
+		self.assertAlmostEqual(out[1][0], 0.2513888889)
+
+		self.assertAlmostEqual(out[0][1], 735502)
+		self.assertAlmostEqual(out[1][1], 0.2527777777)
+
+		self.assertAlmostEqual(out[0][2], 735503)
+		self.assertAlmostEqual(out[1][2], 0.254861111)
+
 
 if __name__ == '__main__':
 	unittest.main()
