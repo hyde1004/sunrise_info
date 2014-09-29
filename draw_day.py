ea404978a61bd62f289eae10869cc0051aacf84e
이정구 <hyde1004@gmail.com>
2014-09-29 18:20:08 +0900

    9월달 그리기 완료

--- draw_day.py
+++ draw_day.py
@@ -1,6 +1,8 @@
 import datetime
 import matplotlib.dates
+import matplotlib.pyplot as plt
 import math
+import day_info
 
 class DrawDay():
 	days = []
@@ -21,3 +23,16 @@ class DrawDay():
 		# #return 0
 		# #return ([735501, 735502, 735503], [0.2513888889, 0.2527777777, 0.254861111])
 		return (self.days, self.times)
+
+
+day = day_info.DayInfo('천안')
+day.set_year(2014)
+day.set_month(9)
+day.read_data()
+b = []
+for a in day.data:
+	b.append(int(a.split(':')[0])*60+int(a.split(':')[1]))
+
+#print(b)
+plt.plot(b)
+plt.show()
