5f7d5b4b3f0bc8ad35ea41d7ab8c8e3cee77413d
이정구 <hyde1004@gmail.com>
2014-09-30 13:19:41 +0900

    draw_day 모듈은 원래 그림을 그리는 모듈이었으나, 모듈 대신  Application 으로 만든다. 그리고, 배웠던 내용을 READ.md로 옮겨놓았다.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 일출몰 시간을 읽어와서 그래프로 그리고,
 오늘의 일출몰 시간을 표시한다.
+
+#### 참고할 것
+  - math.trunc()
+  - datetime.date
+  - matplotlib.dates.date2num()
--- draw_day.py
+++ draw_day.py
@@ -1,28 +1,5 @@
-import datetime
-import matplotlib.dates
 import matplotlib.pyplot as plt
-import math
 import day_info
-
-class DrawDay():
-	days = []
-	times = []
-
-	def __init__(self):
-		pass
-
-	def get_data(self, day_info):
-		for day in day_info:
-			info = math.modf(matplotlib.dates.date2num(day))
-
-			self.times.append(info[0])
-			self.days.append(info[1])
-		# self.days = [math.trunc(matplotlib.dates.date2num(day)) for day in day_info]
-		# #self.days = [datetime.date(day.year, day.month, day.day) for day in day_info]
-		# self.times = [matplotlib.dates.date2num(day) - math.trunc(matplotlib.dates.date2num(day))  for day in day_info]
-		# #return 0
-		# #return ([735501, 735502, 735503], [0.2513888889, 0.2527777777, 0.254861111])
-		return (self.days, self.times)
 
 
 day = day_info.DayInfo('천안')
